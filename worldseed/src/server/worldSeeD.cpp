199847a11219970ee8737bda78a12f9940b3d433
Skyler Sully <skyler.o.sully@gmail.com>
2017-05-02 20:06:42 -0400

    Stlll playing with tcp error for worldSeed-server

--- worldseed/src/server/worldSeeD.cpp
+++ worldseed/src/server/worldSeeD.cpp
@@ -1,27 +1,41 @@
 #include <zmq.hpp>
 #include <iostream>
+#include <string>
 
-int main( int argc, char** argv ) {
+#ifndef _WIN32
+    #include <unistd.h>
+#else
+    #include <windows.h>
+    #define sleep(n)    Sleep(n)
+#endif
+
+int main( int argc, char **argv ) {
     zmq::context_t context( 1 /* I/O threads */ );
     zmq::socket_t  socket( context, ZMQ_REP );
-    socket.bind( "tcp://lo:5555" );
+    socket.bind( "tcp://*:5555" );
 
     std::cout << "///////////////////////////////////////////////////" << std::endl;
     std::cout << "// W o r l d S e e D  ZeroMQ Test /////////////////" << std::endl;
     std::cout << "///////////////////////////////////////////////////" << std::endl;
 
-    do {{{
-        // wait for next request from client
+    do /* main execution loop */ {{{
+        // send a reply back to client
+        zmq::message_t reply( 13 );
+        memcpy( reply.data(), "Message rcvd!", 13 );
+        //socket.send( reply );
+
         zmq::message_t request;
+
+        // wait for next request from client
         socket.recv( &request );
+        socket.send( reply );
         std::cout << "*** received: " << request.data() << std::endl;
 
         // do some stuff
         sleep( 1 );
 
         // send a reply back to client
-        zmq::message_t reply( 13 );
-        //memcpy( reply.data(), "Message rcvd!", 13 );
+        //zmq::message_t reply( 13 );
         memcpy( reply.data(), "*** WorldSeeD", 13 );
         socket.send( reply );
     }}} while( true );
