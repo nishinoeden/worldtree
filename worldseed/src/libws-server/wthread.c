f7f340db65d36d2a9dbf3b0bc65a015ebba1a852
Skyler Sully <skyler.o.sully@gmail.com>
2017-05-18 13:12:33 -0400

    Added comments for the wthread lib.

--- worldseed/src/libws-server/wthread.c
+++ worldseed/src/libws-server/wthread.c
@@ -2,25 +2,39 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+//#include <stdbool.h>
 #include <string.h>
 #include <pthread.h>
 #include <assert.h>
 #include <zmq.h>
 
-
 #define NUM_THRD 5    /* number of threads */
 
-//typedef struct device device_t;
+/**
+ * typedef struct device device_t;
+ *
+ * device_t contains the structure for the zmq context
+ * sockets, dealers, and routers.
+ *
+ * @var void* context the zmq context
+ * @var void* socket  the zmq socket struct
+ */
 struct device {
     void      *context;
     void      *socket;
 };
 
 /**
- * worker_t struct contains the structure for all worker threads.
+ * typedef struct worker worker_t;
+ *
+ * worker_t struct contains the structure for the worker
+ * thread that handles incoming socket connections and
+ * handling all requests.
+ *
+ * @var pthread_t thread the worker thread
+ * @var device_t device the device structure
+ * @var int id the ID for the thread
  */
-//typedef struct worker worker_t;
 struct worker {
     pthread_t  thread;
     device_t   device;
@@ -29,6 +43,13 @@ struct worker {
     int        id;
 };
 
+/**
+ * ws_wtd_new() creates a new worker_t struct and returns
+ * it to the calling method. All member variables are
+ * initialized before returning.
+ *
+ * @return worker_t*
+ */
 worker_t *ws_wtd_new() {
     worker_t *wthread = malloc( sizeof(worker_t) );
     int       rc, tLimit;
@@ -41,6 +62,13 @@ worker_t *ws_wtd_new() {
     return wthread;
 }
 
+/**
+ * ws_wtd_start() spawns a new thread, calling the
+ * ws_wtd_exec function which manages the worker thread
+ *
+ * @param worker_t* worker The worker thread struct
+ * @return void* null
+ */
 void *ws_wtd_start( worker_t *worker ) {
     int      rc;
     long     i = 0;
@@ -56,6 +84,15 @@ void *ws_wtd_start( worker_t *worker ) {
     pthread_join( worker->thread, NULL );
 }
 
+/**
+ * ws_wtd_exec() is called by the pthread_create function,
+ * and is responsible for creating the zmq context, binding to,
+ * and handling all incoming connections on a specified address
+ * and port.
+ *
+ * @param void* wtd A reference to a worker_t struct
+ * @return void* null
+ */
 void *ws_wtd_exec( void *wtd ) {
     // create a new context and socket
     worker_t *worker = (worker_t *)wtd;
@@ -72,6 +109,14 @@ void *ws_wtd_exec( void *wtd ) {
     pthread_exit( NULL );
 }
 
+/**
+ * ws_wtd_listen() handles all requests from the client, passing
+ * the request off to the dispatcher, which will handle all of
+ * the background processing for all requests.
+ *
+ * @param worker_t* worker The worker thread struct
+ * @return void* null
+ */
 void *ws_wtd_listen( worker_t *worker ) {
     int      numRequest = 0;
 
@@ -94,6 +139,11 @@ void *ws_wtd_listen( worker_t *worker ) {
     return NULL;
 }
 
+/**
+ * Prints the header and ZMQ version to stdout
+ *
+ * @return void* null
+ */
 void* ws_print_header() {
     int  major, minor, patch;
 
--- worldseed/src/libws-server/wthread.h
+++ worldseed/src/libws-server/wthread.h
@@ -1,11 +1,67 @@
 #include <stdio.h>
 
+/**
+ * device_t contains the structure for the zmq context
+ * sockets, dealers, and routers.
+ *
+ * @var void* context the zmq context
+ * @var void* socket  the zmq socket struct
+ */
 typedef struct device device_t;
 
+/**
+ * worker_t struct contains the structure for the worker
+ * thread that handles incoming socket connections and
+ * handling all requests.
+ *
+ * @var pthread_t thread the worker thread
+ * @var device_t device the device structure
+ * @var int id the ID for the thread
+ */
 typedef struct worker worker_t;
 
+/**
+ * ws_wtd_new() creates a new worker_t struct and returns
+ * it to the calling method. All member variables are
+ * initialized before returning.
+ *
+ * @return worker_t*
+ */
 worker_t *ws_wtd_new();
+
+/**
+ * ws_wtd_start() spawns a new thread, calling the
+ * ws_wtd_exec function which manages the worker thread
+ *
+ * @param worker_t* worker The worker thread struct
+ * @return void* null
+ */
 void *ws_wtd_start( worker_t *worker );
+
+/**
+ * ws_wtd_exec() is called by the pthread_create function,
+ * and is responsible for creating the zmq context, binding to,
+ * and handling all incoming connections on a specified address
+ * and port.
+ *
+ * @param void* wtd A reference to a worker_t struct
+ * @return void* null
+ */
 void *ws_wtd_exec( void *wtd );
+
+/**
+ * ws_wtd_listen() handles all requests from the client, passing
+ * the request off to the dispatcher, which will handle all of
+ * the background processing for all requests.
+ *
+ * @param worker_t* worker The worker thread struct
+ * @return void* null
+ */
 void *ws_wtd_listen( worker_t *worker );
+
+/**
+ * Prints the header and ZMQ version to stdout
+ *
+ * @return void* null
+ */
 void *ws_print_header();
